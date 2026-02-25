f340df2fba94b577ebdeea26ab68720e0c54d9c1
Jude Pereira <jude@clevertap.com>
2026-02-25 14:34:32 +0100

    append current user to .bashrc if not already configured.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,16 @@
 set -euo pipefail
 
 TAG_BIN="# from entrypoint.sh: add bin"
+TAG_USER="# from entrypoint.sh: add user"
 
 if ! cat ~/.bashrc | grep "$TAG_BIN"; then
     echo "$TAG_BIN" >> ~/.bashrc
     echo "export PATH=~/bin:\$PATH" >> ~/.bashrc
+fi
+
+if ! cat ~/.bashrc | grep "$TAG_USER"; then
+    echo "$TAG_USER" >> ~/.bashrc
+    echo "export USER=$(whoami)" >> ~/.bashrc
 fi
 
 curl -fsSL https://opencode.ai/install | bash
