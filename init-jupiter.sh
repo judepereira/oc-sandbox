8c5b8b2447acc33dd000f6e16811e352a1c59ce2
Jude Pereira <contact@judepereira.com>
2026-09-05 08:55:52 +0200

    add more playwright deps.

--- init-jupiter.sh
+++ init-jupiter.sh
@@ -6,6 +6,12 @@ apt install -y git wget curl htop vim build-essential sudo \
     libxrandr2 libxss1 libxtst6 libasound2t64 libatk-bridge2.0-0 libatspi2.0-0 \
     libcups2 libgbm1 libdrm2 libxkbcommon0 libssl3 jq ripgrep
 
+# Additional playwright deps.
+apt install libavif16 libenchant-2-2 libevent-2.1-7t64 libflite1 libgles2 \
+    libgraphene-1.0-0 libgstreamer-plugins-base1.0-0 libgstreamer1.0-0 libgtk-4-1 \
+    libharfbuzz-icu0 libhyphen0 libicu78 libjpeg8 libmanette-0.2-0 libopus0 libsecret-1-0 libvulkan1 \
+    libwayland-server0 libwebp7 libwebpdemux2 libwebpmux3 libxslt1.1 libx264-16
+
 mkdir -p /home/jude/.jdks
 chown -R jude:jude /home/jude/.jdks
 chown jude:jude /home/jude # don't -R since there are volumes mounted underneath
